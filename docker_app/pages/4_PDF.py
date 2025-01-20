772449a2752bfaa1a70f86d0d0cd6bebae7c8fb0
Derek Stearns <djstearns402@gmail.com>
2025-01-20 07:20:24 -0600

    moving files

--- docker_app/pages/4_PDF.py
+++ docker_app/pages/4_PDF.py
@@ -21,29 +21,9 @@ st.sidebar.header("PDF")
 
 st.markdown("# PDF")
 
-# ID of Secrets Manager containing cognito parameters
-secrets_manager_id = Config.SECRETS_MANAGER_ID
-
-# ID of the AWS region in which Secrets Manager is deployed
-region = Config.DEPLOYMENT_REGION
-
-
-# Initialise CognitoAuthenticator
-authenticator = Auth.get_authenticator(secrets_manager_id, region)
-
-# Authenticate user, and stop here if not logged in
-is_logged_in = authenticator.login()
-if not is_logged_in:
-    st.stop()
-
-
-def logout():
-    authenticator.logout()
-
 with st.sidebar:
-    st.text(f"Welcome,\n{authenticator.get_username()}")
-    st.button("Logout", "logout_btn", on_click=logout)
-
+    st.text(f"Welcome!")
+  
 # st.write(
 #     """This demo illustrates a combination of plotting and animation with
 # Streamlit. We're generating a bunch of random numbers in a loop for around
